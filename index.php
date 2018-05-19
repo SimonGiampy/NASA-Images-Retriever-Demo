7c0b91c76dbf867f28d4ab03d32bc6e049d365df
SimonGiampy <simo.diablo8@gmail.com>
2018-05-19 17:46:15 +0200

    added dependencies for future use

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>NASA API</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>NASA API</title>
 </head>
 <body>
 	<form method="get" name="query" action="get-images.php">
