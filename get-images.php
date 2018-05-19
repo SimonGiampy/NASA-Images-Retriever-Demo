038aa94ee234467e97cc57b7b176062e7927f79b
SimonGiampy <simo.diablo8@gmail.com>
2018-05-19 11:54:48 +0200

    fetches all the elements from the get request
    fetches all the elements and displays on the screen different values for each image (title, description, keywords used, image href). each images' details are contained into paragraphs

--- get-images.php
+++ get-images.php
@@ -6,10 +6,39 @@ $sampleQuery = "https://images-api.nasa.gov/search?q=etna&media_type=image";
 $curl = curl_init($sampleQuery);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $json = curl_exec($curl);
-$decodeJson = json_decode($json, true);
+$decodedJson = json_decode($json, true);
+
+echo "<p>";
 
 //returns total number of results
-$totalHits = $response["collection"]["metadata"]["total_hits"];
+$totalHits = $decodedJson["collection"]["metadata"]["total_hits"];
+echo "total hits: ".$totalHits."<br />";
 
+$items = $decodedJson["collection"]["items"];
+for ($i = 0; $i < $totalHits; $i++) {
+    //title
+    $title = $items[$i]["data"][0]["title"];
+    echo "title ".$i.": ".$title."<br />";
+
+    //description
+    $description = $items[$i]["data"][0]["description"];
+    echo "description ".$i.": ".$description."<br />";
+
+    //keywords
+    $keywords = $items[$i]["data"][0];
+    if (array_key_exists("keywords", $keywords)) {
+        $keywords = $keywords["keywords"];
+    }
+    foreach ($keywords as $keyword) {
+        echo $keyword." ";
+    }
+    echo "<br />";
+
+    //href image thumb / original
+    $previewImage = $items[$i]["links"][0]["href"];
+    //$previewImage = str_replace("thumb", "orig", $previewImage);
+    echo "preview image link ".$i.": ".$previewImage."<br />";
+}
+echo "</p>";
 //close curl request
 curl_close($curl);
